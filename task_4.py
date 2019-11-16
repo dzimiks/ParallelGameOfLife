2fa1b5f41c119fd152cdde16b588af25686b18c0
Vanja Paunović <vana997@gmail.com>
2019-11-16 01:01:09 +0100

    Finish Task 4

--- task_4.py
+++ task_4.py
@@ -11,77 +11,89 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from multiprocessing import Pool, Process
-from time import sleep
 import json
+from multiprocessing import Pool, cpu_count
 
-row_counter = 0
-N = 3
+
+ITERATIONS = 20
+N = 20
 ALIVE = 255
 DEAD = 0
 values = [ALIVE, DEAD]
-grid = np.random.choice(values, N ** 2).reshape(N, N)
+grid = np.random.choice(values, N ** 2, p=(0.2, 0.8)).reshape(N, N)
 
 
-class Cell(Process):
-    def __init__(self, x, y, state):
-        super().__init__()
-        self.x = x
-        self.y = y
-        self.state = state
+def play_game(task, grid, N):
+    coordinates = {}
 
-    def fun(self, grid):
-        print(f'x: {self.x}, y: {self.y}, state: {self.state}')
-        print(grid)
-
-
-def game(grid_row):
-    # global row_counter
-    column = 0
-    sleep(0.5)
-
-    print(grid_row)
-    print()
-
-    # for cell in grid:
-    print('N:', N)
-    print('row_counter:', row_counter)
-    print('column:', column)
-    print()
-
-    for _ in grid_row:
+    for x, y in task:
         result = {
-            'upper_left': grid[(row_counter - 1) % N, (column - 1) % N],
-            'upper': grid[(row_counter - 1) % N, column],
-            'upper_right': grid[(row_counter - 1) % N, (column + 1) % N],
-            'left': grid[row_counter, (column - 1) % N],
-            'center': grid[row_counter, column],
-            'right': grid[row_counter, (column + 1) % N],
-            'lower_left': grid[(row_counter + 1) % N, (column - 1) % N],
-            'lower': grid[(row_counter + 1) % N, column],
-            'lower_right': grid[(row_counter + 1) % N, (column + 1) % N]
+            'upper_left': grid[(x - 1) % N, (y - 1) % N],
+            'upper': grid[(x - 1) % N, y],
+            'upper_right': grid[(x - 1) % N, (y + 1) % N],
+            'left': grid[x, (y - 1) % N],
+            # 'center': grid[x, y],
+            'right': grid[x, (y + 1) % N],
+            'lower_left': grid[(x + 1) % N, (y - 1) % N],
+            'lower': grid[(x + 1) % N, y],
+            'lower_right': grid[(x + 1) % N, (y + 1) % N]
         }
 
-        column += 1
-        print(json.dumps(result, default=str, indent=4))
+        # print(json.dumps(result, default=str, indent=4))
 
+        total = 0
 
-def program():
-    global row_counter
+        for v in result.values():
+            total += v
 
-    print(grid)
+        total //= 255
 
-    pool = Pool(N)
-    print('Pool kreiran')
+        # Conway's rules
+        if grid[x, y] == ALIVE:
+            if total < 2 or total > 3:
+                coordinates[(x, y)] = DEAD
+            else:
+                coordinates[(x, y)] = ALIVE
+        else:
+            if total == 3:
+                coordinates[(x, y)] = ALIVE
+            else:
+                coordinates[(x, y)] = DEAD
 
-    for i in range(N):
-        pool.map(game, grid[i:i + 1, :])
-        print('map() pozvan')
-        row_counter += 1
-
-    pool.close()
+    return coordinates
 
 
 if __name__ == '__main__':
-    program()
+    matrix_list = [grid.copy()]
+    tasks = []
+    results = []
+
+    print(grid)
+    print()
+
+    # Add grid rows
+    for i in range(N):
+        row = []
+        for j in range(N):
+            row.append((i, j))
+        tasks.append(row)
+
+    pool = Pool(cpu_count())
+
+    for i in range(ITERATIONS):
+        results = [pool.apply(play_game, args=(task, grid, N,)) for task in tasks]
+        # results.append(result)
+        # print('results:', results)
+
+        for r in results:
+            for k, v in r.items():
+                grid[k[0], k[1]] = v
+
+        matrix_list.append(grid.copy())
+
+    pool.close()
+    pool.join()
+
+    for matrix in matrix_list:
+        plt.imshow(matrix, interpolation='nearest')
+        plt.show()
